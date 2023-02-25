608b0f2b67a037aeffd6b09d19ee18b55f7746c0
Samarth Bagga <samarthbagga@gmail.com>
2023-02-25 20:00:14 +0530

    trying vs code commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import { TailwindProvider } from 'nativewind';
+
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500 p-10">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
