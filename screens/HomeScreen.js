e3010d6b94d9df2bc193f9d6c6b20afb8e5500f3
Samarth Bagga <samarthbagga@gmail.com>
2023-03-01 16:52:46 +0530

    did stuff again

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,6 +4,12 @@ import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useLayoutEffect } from 'react';
 import { Image } from 'react-native';
+import {ChevronDownIcon,
+        UserIcon,
+        SearchIcon,
+        AdjustmentsIcon,
+        TextInput,
+} from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
 
@@ -19,10 +25,24 @@ const HomeScreen = () => {
         <Image source={{url:"https://links.papareact.com/wru"}}
         className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-          <Text className="font-bold text-xl">Current Location</Text>
+          <Text className="font-bold text-xl">Current Location
+          <ChevronDownIcon size={20} color="#00CCBB"/>
+          </Text>
         </View>
+        <UserIcon size={35} color="#00CCBB"/>
+      </View>
+
+
+      <View>
+        <View>
+         <SearchIcon/>
+         <TextInput placeholder="restaurants and cuisines"
+         keyboardType="default"
+         /> 
+        </View>
+        <AdjustmentsIcon color="#00CCBB"/>
       </View>
 
     </SafeAreaView>
